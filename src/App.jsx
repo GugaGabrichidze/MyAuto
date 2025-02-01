d1d1e030a5468a8992c9a84d0275e76f652bb461
GugaGabrichidze <guga.gabrichidze777@gmail.com>
2025-02-01 20:33:59 +0400

    MyAuto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import SearchBar from "./searchbar/SearchBar.jsx";
+
+function App() {
+
+  return (
+    <>
+        <SearchBar/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/searchbar/SearchBar.jsx
@@ -0,0 +1,34 @@
+import './searchBar.css'
+function SearchBar(){
+    return(
+        <>
+            <div className={"main"}>
+                <div className={"carButtons"}></div>
+                <button className={"carbutton"}><img src={"src/searchbar/Logos/car.png"}></img></button>
+                <button className={"tractorbutton"}><img src={"src/searchbar/Logos/tractor.png"}></img></button>
+                <button className={"motobutton"}> <img src={"src/searchbar/Logos/moto.png"}></img></button>
+                <div className={"selects"}>
+                    <p className={"selectname"}>გარიგების ტიპი</p>
+                    <div><select><option>იყიდება</option></select></div>
+                    <div><p className={"selectname"}>მწარმოებელი</p></div>
+                    <div><select><option>ყველა მწარმოებელი</option></select></div>
+                    <div><p className={"selectname"}>კატეგორია</p></div>
+                    <div><select><option>ყველა კატეგორია</option></select></div>
+                </div>
+                <div className={"fasi"}>
+                <div><p>ფასი</p>
+                    <button>Euro</button>
+                    <button>Gel</button>
+                </div>
+                    <div className={"inputsprices"}></div>
+                <input type={"text"} placeholder={"დან"}></input>
+                <input type={"text"} placeholder={"მდე"}></input>
+                </div>
+                <button>ძებნა</button>
+            </div>
+
+        </>
+    )
+}
+
+export default SearchBar;
